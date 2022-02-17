4a9d931a157d522dc15047b32a74b110b54470d7
Diana Marcelin <marcelindiana@yahoo.com>
2022-02-17 10:57:18 -0500

    framed out api

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors"); //allow front-end
+const { getTasks } = require("./src/tasks");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors()); // and back-end to connect
+app.use(express.json());
+
+//Routes
+// app.post("/tasks", createTask);
+
+app.get("/tasks", getTasks);
+
+// app.patch("/tasks/:taskId", updateTask);
+
+app.listen(PORT, () => {
+  console.log("listening on port:", PORT);
+});
---
+++ src/tasks.js
@@ -0,0 +1,9 @@
+// exports.createTask = (req,res) => {
+
+// };
+
+exports.getTasks = (req, res) => {
+  res.send("Get task is working!");
+};
+
+// exports.updateTask = (req,res) => {};
