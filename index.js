00b75754a9136fd540f8dcd97b08fab22a8a97b6
Diana Marcelin <marcelindiana@yahoo.com>
2022-02-21 17:51:27 -0500

    get deletes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors"); //allow front-end
-const { getTasks, createTask, updateTask } = require("./src/tasks");
+const { getTasks, createTask, updateTask, deleteTask } = require("./src/tasks");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -13,6 +13,7 @@ app.post("/tasks", createTask);
 app.get("/tasks", getTasks);
 
 app.patch("/tasks/:taskId", updateTask);
+app.delete("/tasks/:taskId", deleteTask);
 
 app.listen(PORT, () => {
   console.log("listening on port:", PORT);
--- src/tasks.js
+++ src/tasks.js
@@ -2,7 +2,11 @@ const { response } = require("express");
 const { connectDb } = require("./connectDb");
 
 exports.createTask = (req, res) => {
-  const newTask = req.body;
+  const newTask = {
+    task: req.body.task,
+    done: false,
+    deleted: false,
+  };
   const db = connectDb();
   db.collection("tasks")
     .add(newTask)
@@ -24,14 +28,26 @@ exports.getTasks = (req, res) => {
     })
     .catch((err) => res.status(500).send(err));
 };
-
+//Returns Everything
 exports.updateTask = (req, res) => {
   const { taskId } = req.params;
-  const isDone = req.body.isDone;
+  const isDone = req.body.done;
   const db = connectDb();
   db.collection("tasks")
     .doc(taskId)
     .update({ done: isDone })
     .then((doc) => res.status(202).send(doc))
+    .catch((err) => res.status(500).send(err));
+};
+
+exports.deleteTask = (req, res) => {
+  const { taskId } = req.params;
+  const db = connectDb();
+  db.collection("tasks")
+    .doc(taskId)
+    .delete()
+    .then(() => {
+      res.send("Deleted");
+    })
     .catch((err) => res.status(500).send(err));
 };
